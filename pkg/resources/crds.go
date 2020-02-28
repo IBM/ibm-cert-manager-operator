7d85cde5d8c3bfc56fabe43086846b2350e039d4
Crystal Chun <crystal.chun@ibm.com>
2020-02-28 11:07:05 -0500

    More description fixes for issuer.

--- pkg/resources/crds.go
+++ pkg/resources/crds.go
@@ -1222,7 +1222,7 @@ var issuerCRD = &apiext.CustomResourceDefinition{
 																								Description: "Describes pod affinity scheduling rules (e.g. co-locate this pod in the same node, zone, etc. as some other pod(s)).",
 																								Properties: map[string]apiext.JSONSchemaProps{
 																									"preferredDuringSchedulingIgnoredDuringExecution": apiext.JSONSchemaProps{
-																										Description: "The scheduler will prefer to schedule pods to nodes that satisfy the affinity expressions specified by this field, but it may choose a node that violates one or more of the expressions. The node that is most preferred is the one with the greatest sum of weights, i.e. for each node that meets all of the scheduling requirements (resource request, requiredDuringScheduling affinity expressions, etc.), compute a sum by iterating through the elements of this field and adding \"weight\" to the sum if the node matches the corresponding podAffinityTerms; the node(s) with the highest sum are the most preferred.",
+																										Description: "The scheduler will prefer to schedule pods to nodes that satisfy the affinity expressions specified by this field, but it may choose a node that violates one or more of the expressions. The node that is most preferred is the one with the greatest sum of weights, i.e. for each node that meets all of the scheduling requirements (resource request, requiredDuringScheduling affinity expressions, etc.), compute a sum by iterating through the elements of this field and adding \"weight\" to the sum if the node has pods which matches the corresponding podAffinityTerm; the node(s) with the highest sum are the most preferred.",
 																										Items: &apiext.JSONSchemaPropsOrArray{
 																											Schema: &apiext.JSONSchemaProps{
 																												Description: "The weights of all of the matched WeightedPodAffinityTerm fields are added per-node to find the most preferred node(s)",
@@ -1388,7 +1388,7 @@ var issuerCRD = &apiext.CustomResourceDefinition{
 																												Description: "The weights of all of the matched WeightedPodAffinityTerm fields are added per-node to find the most preferred node(s)",
 																												Properties: map[string]apiext.JSONSchemaProps{
 																													"podAffinityTerm": apiext.JSONSchemaProps{
-																														Description: "Required. A pod affinity term, associated with the corresponding weight..",
+																														Description: "Required. A pod affinity term, associated with the corresponding weight.",
 																														Properties: map[string]apiext.JSONSchemaProps{
 																															"labelSelector": apiext.JSONSchemaProps{
 																																Description: "A label query over a set of resources, in this case pods.",
@@ -1453,7 +1453,7 @@ var issuerCRD = &apiext.CustomResourceDefinition{
 																														Type:     "object",
 																													},
 																													"weight": apiext.JSONSchemaProps{
-																														Description: "Weight associated with matching the corresponding nodeSelectorTerm, in the range 1-100.",
+																														Description: "Weight associated with matching the corresponding podAffinityTerm, in the range 1-100.",
 																														Format:      "int32",
 																														Type:        "integer",
 																													},
