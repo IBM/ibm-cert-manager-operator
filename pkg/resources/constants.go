d0841701beb1b40c529233c10587ce3e819b602a
swati-nair <59711259+swati-nair@users.noreply.github.com>
2020-04-27 10:45:24 -0700

    productVersion updated to 3.4.0 in metering annotations

--- pkg/resources/constants.go
+++ pkg/resources/constants.go
@@ -81,12 +81,12 @@ var ConfigmapWatcherLabelMap = map[string]string{
 }
 
 // PodAnnotations are the annotations required for a pod
-var PodAnnotations = map[string]string{"openshift.io/scc": "restricted", "productName": "IBM Cloud Platform Common Services", "productID": "068a62892a1e4db39641342e592daa25", "productVersion": "3.3.0", "productMetric": "FREE"}
+var PodAnnotations = map[string]string{"openshift.io/scc": "restricted", "productName": "IBM Cloud Platform Common Services", "productID": "068a62892a1e4db39641342e592daa25", "productVersion": "3.4.0", "productMetric": "FREE"}
 
 var securityAnnotationWebhook = map[string]string{"openshift.io/scc": "hostnetwork",
 	"productName":    "IBM Cloud Platform Common Services",
 	"productID":      "068a62892a1e4db39641342e592daa25",
-	"productVersion": "3.3.0",
+	"productVersion": "3.4.0",
 	"productMetric":  "FREE",
 }
 
