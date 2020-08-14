9529c9227f7377fd1830f2bd7904f5e3d2e1a93c
swati-nair <59711259+swati-nair@users.noreply.github.com>
2020-08-14 13:20:31 -0700

    update go version to 1.14 and update operand shas

--- common/scripts/get_image_sha_digest.sh
+++ common/scripts/get_image_sha_digest.sh
@@ -17,6 +17,17 @@
 # Get the SHA from the RepoDigests section of an operand image.
 # Do "docker login" before running this script.
 # Run this script from the parent dir by typing "scripts/get-image-sha.sh"
+# Do "docker login" before running this script.
+
+SED="sed"
+unamestr=$(uname)
+if [[ "$unamestr" == "Darwin" ]] ; then
+    SED=gsed
+    type $SED >/dev/null 2>&1 || {
+        echo >&2 "$SED it's not installed. Try: brew install gnu-sed" ;
+        exit 1;
+    }
+fi
 
 FILE=deploy/operator.yaml
 
@@ -27,7 +38,7 @@ TAG=$3
 TYPE=$4
 if [[ $TYPE == "" ]]
 then
-   echo "Missing parm. Need image registry, image name, image tag, and env variable indicating operand type. Tye will be OPERATOR for operator image as input"
+   echo "Missing parm. Need image registry, image name, image tag, and env variable indicating operand type. Type will be OPERATOR for operator image as input"
    echo "for eg: quay.io/opencloudio icp-cert-manager-controller x.y.z CONTROLLER_IMAGE_TAG_OR_SHA"
    exit 1
 fi
@@ -51,14 +62,27 @@ SHA=$DIGEST
 
 echo "$NAME : $SHA"
 
-# delete the "name" and "value" lines for the old SHA ONLY FOR OPERANDS
+# delete the "name" and "value" lines for the old SHA ONLY FOR OPERANDS in current CSV
 # for example:
 #     - name: CONTROLLER_IMAGE_TAG_OR_SHA
 #       value: "sha256:10a844ffaf7733176e927e6c4faa04c2bc4410cf4d4ef61b9ae5240aa62d1456"
 
-sed -i "/name: $TYPE/{N;d;}" $FILE
+CSV_VERSION=$(cat version/version.go | grep "Version =" | awk '{ print $3}' | tr -d '"')
 
-# insert the new SHA lines
-LINE1="\            - name: $TYPE"
-LINE2="\              value: \"$SHA\""
-sed -i "/DO NOT DELETE. Add operand image SHAs here./a $LINE1\n$LINE2" $FILE
+CSV_FILE=deploy/olm-catalog/ibm-cert-manager-operator/${CSV_VERSION}/ibm-cert-manager-operator.v${CSV_VERSION}.clusterserviceversion.yaml
+
+echo "Updating operand $TYPE in $CSV_FILE"
+$SED -i "/name: $TYPE/{N;d;}" "$CSV_FILE"
+
+# insert the new SHA lines. need 4 more leading spaces compared to operator.yaml
+LINE1="\                - name: $TYPE"
+LINE2="\                  value: $SHA"
+$SED -i "/env:/a $LINE1\n$LINE2" "$CSV_FILE"
+
+# Not updating the operands anymore in operator.yaml
+# sed -i "/name: $TYPE/{N;d;}" $FILE
+
+# # insert the new SHA lines
+# LINE1="\            - name: $TYPE"
+# LINE2="\              value: \"$SHA\""
+# sed -i "/DO NOT DELETE. Add operand image SHAs here./a $LINE1\n$LINE2" $FILE
