ce118376453d9dde49e34168d2ac94abe879988c
Henry Li <henry.h.li@ibm.com>
2021-08-11 17:52:06 -0400

    updated go to 1.16
    added converter for v1alpha1 Certificates
    
    reverted dev-csv changes

--- pkg/controller/certificate/certificate_controller.go
+++ pkg/controller/certificate/certificate_controller.go
@@ -14,13 +14,14 @@
 // limitations under the License.
 //
 
-package issuer
+package certificate
 
 import (
 	"context"
 
 	certmanagerv1 "github.com/ibm/ibm-cert-manager-operator/pkg/apis/certmanager/v1"
 	certmanagerv1alpha1 "github.com/ibm/ibm-cert-manager-operator/pkg/apis/certmanager/v1alpha1"
+	cmmeta "github.com/ibm/ibm-cert-manager-operator/pkg/apis/meta/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -34,14 +35,14 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
-var log = logf.Log.WithName("controller_issuer")
+var log = logf.Log.WithName("controller_certificate")
 
 /**
 * USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
 * business logic.  Delete these comments after modifying this file.*
  */
 
-// Add creates a new Issuer Controller and adds it to the Manager. The Manager will set fields on the Controller
+// Add creates a new Certificate Controller and adds it to the Manager. The Manager will set fields on the Controller
 // and Start it when the Manager is Started.
 func Add(mgr manager.Manager) error {
 	return add(mgr, newReconciler(mgr))
@@ -49,28 +50,28 @@ func Add(mgr manager.Manager) error {
 
 // newReconciler returns a new reconcile.Reconciler
 func newReconciler(mgr manager.Manager) reconcile.Reconciler {
-	return &ReconcileIssuer{client: mgr.GetClient(), scheme: mgr.GetScheme()}
+	return &ReconcileCertificate{client: mgr.GetClient(), scheme: mgr.GetScheme()}
 }
 
 // add adds a new Controller to mgr with r as the reconcile.Reconciler
 func add(mgr manager.Manager, r reconcile.Reconciler) error {
 	// Create a new controller
-	c, err := controller.New("issuer-controller", mgr, controller.Options{Reconciler: r})
+	c, err := controller.New("certificate-controller", mgr, controller.Options{Reconciler: r})
 	if err != nil {
 		return err
 	}
 
-	// Watch for changes to primary resource Issuer
-	err = c.Watch(&source.Kind{Type: &certmanagerv1alpha1.Issuer{}}, &handler.EnqueueRequestForObject{})
+	// Watch for changes to primary resource Certificate
+	err = c.Watch(&source.Kind{Type: &certmanagerv1alpha1.Certificate{}}, &handler.EnqueueRequestForObject{})
 	if err != nil {
 		return err
 	}
 
 	// TODO(user): Modify this to be the types you create that are owned by the primary resource
-	// Watch for changes to secondary resource Pods and requeue the owner Issuer
+	// Watch for changes to secondary resource Pods and requeue the owner Certificate
 	err = c.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForOwner{
 		IsController: true,
-		OwnerType:    &certmanagerv1alpha1.Issuer{},
+		OwnerType:    &certmanagerv1alpha1.Certificate{},
 	})
 	if err != nil {
 		return err
@@ -79,30 +80,30 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 	return nil
 }
 
-// blank assignment to verify that ReconcileIssuer implements reconcile.Reconciler
-var _ reconcile.Reconciler = &ReconcileIssuer{}
+// blank assignment to verify that ReconcileCertificate implements reconcile.Reconciler
+var _ reconcile.Reconciler = &ReconcileCertificate{}
 
-// ReconcileIssuer reconciles a Issuer object
-type ReconcileIssuer struct {
+// ReconcileCertificate reconciles a Certificate object
+type ReconcileCertificate struct {
 	// This client, initialized using mgr.Client() above, is a split client
 	// that reads objects from the cache and writes to the apiserver
 	client client.Client
 	scheme *runtime.Scheme
 }
 
-// Reconcile reads that state of the cluster for a Issuer object and makes changes based on the state read
-// and what is in the Issuer.Spec
+// Reconcile reads that state of the cluster for a Certificate object and makes changes based on the state read
+// and what is in the Certificate.Spec
 // TODO(user): Modify this Reconcile function to implement your Controller logic.  This example creates
 // a Pod as an example
 // Note:
 // The Controller will requeue the Request to be processed again if the returned error is non-nil or
 // Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
-func (r *ReconcileIssuer) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+func (r *ReconcileCertificate) Reconcile(request reconcile.Request) (reconcile.Result, error) {
 	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
-	reqLogger.Info("Reconciling Issuer")
+	reqLogger.Info("Reconciling Certificate")
 
-	// Fetch the Issuer instance
-	instance := &certmanagerv1alpha1.Issuer{}
+	// Fetch the Certificate instance
+	instance := &certmanagerv1alpha1.Certificate{}
 	err := r.client.Get(context.TODO(), request.NamespacedName, instance)
 	if err != nil {
 		if errors.IsNotFound(err) {
@@ -115,18 +116,15 @@ func (r *ReconcileIssuer) Reconcile(request reconcile.Request) (reconcile.Result
 		return reconcile.Result{}, err
 	}
 
-	reqLogger.Info("### DEBUG ### v1alpha1 Issuer created", "Issuer.Namespace", instance.Namespace, "Issuer.Name", instance.Name)
+	reqLogger.Info("### DEBUG ### v1alpha1 Certificate created", "Certificate.Namespace", instance.Namespace, "Certificate.Name", instance.Name)
 
-	reqLogger.Info("### DEBUG ### Creating v1 Issuer", "Issuer.Namespace", instance.Namespace, "Issuer.Name", instance.Name)
+	reqLogger.Info("### DEBUG ### Creating v1 Certificate", "Certificate.Namespace", instance.Namespace, "Certificate.Name", instance.Name)
 
 	annotations := instance.Annotations
 	annotations["ibm-cert-manager-operator-generated"] = "true"
 
-	v1Issuer := certmanagerv1.Issuer{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Issuer",
-			APIVersion: "cert-manager.io/v1",
-		},
+	certificate := certmanagerv1.Certificate{
+		TypeMeta: metav1.TypeMeta{Kind: "Certificate", APIVersion: "cert-manager.io/v1"},
 		ObjectMeta: metav1.ObjectMeta{
 			Name:            instance.Name,
 			Namespace:       instance.Namespace,
@@ -134,23 +132,29 @@ func (r *ReconcileIssuer) Reconcile(request reconcile.Request) (reconcile.Result
 			Annotations:     annotations,
 			OwnerReferences: instance.OwnerReferences,
 		},
-		Spec: certmanagerv1.IssuerSpec{
-			IssuerConfig: certmanagerv1.IssuerConfig{
-				SelfSigned: &certmanagerv1.SelfSignedIssuer{},
+		Spec: certmanagerv1.CertificateSpec{
+			CommonName: instance.Spec.CommonName,
+			Duration:   &metav1.Duration{Duration: instance.Spec.Duration.Duration},
+			IssuerRef: cmmeta.ObjectReference{
+				Name:  instance.Spec.IssuerRef.Name,
+				Kind:  instance.Spec.IssuerRef.Kind,
+				Group: instance.Spec.IssuerRef.Group,
 			},
+			IsCA:        instance.Spec.IsCA,
+			RenewBefore: &metav1.Duration{Duration: instance.Spec.RenewBefore.Duration},
+			SecretName:  instance.Spec.SecretName,
 		},
 	}
-
-	if err := r.client.Create(context.TODO(), &v1Issuer); err != nil {
+	if err := r.client.Create(context.TODO(), &certificate); err != nil {
 		if errors.IsAlreadyExists(err) {
-			reqLogger.Error(err, "### DEBUG ### v1 Issuer already exists")
+			reqLogger.Error(err, "### DEBUG ### v1 Certificate already exists")
 			return reconcile.Result{}, nil
 		}
-		reqLogger.Error(err, "### DEBUG ### Failed to create v1 Issuer")
+		reqLogger.Error(err, "### DEBUG ### Failed to create v1 Certificate")
 		return reconcile.Result{}, err
 	}
 
-	reqLogger.Info("### DEBUG #### Created v1 Issuer")
+	reqLogger.Info("### DEBUG #### Created v1 Certificate")
 
 	return reconcile.Result{}, nil
 }
