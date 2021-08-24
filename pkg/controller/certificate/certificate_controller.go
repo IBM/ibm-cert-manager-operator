546bd32146d26efa8cbf7f41e1ac20614047022b
Jiaming Hu <horis233@gmail.com>
2021-08-24 18:16:44 -0400

    feat: Set v1alpha1 objects as the owner of v1 objects (#152)
    * feat: Set v1alpha1 certificates and issuers as owners of v1 certificates and issuers
    
    * compare before update and don't override annotation during update
    
    * remove owner reference in cert

--- pkg/controller/certificate/certificate_controller.go
+++ pkg/controller/certificate/certificate_controller.go
@@ -19,20 +19,23 @@ package certificate
 import (
 	"context"
 
-	certmanagerv1 "github.com/ibm/ibm-cert-manager-operator/pkg/apis/certmanager/v1"
-	certmanagerv1alpha1 "github.com/ibm/ibm-cert-manager-operator/pkg/apis/certmanager/v1alpha1"
 	cmmeta "github.com/ibm/ibm-cert-manager-operator/pkg/apis/meta/v1"
-	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/equality"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	certmanagerv1 "github.com/ibm/ibm-cert-manager-operator/pkg/apis/certmanager/v1"
+	certmanagerv1alpha1 "github.com/ibm/ibm-cert-manager-operator/pkg/apis/certmanager/v1alpha1"
 )
 
 var log = logf.Log.WithName("controller_certificate")
@@ -69,7 +72,7 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 
 	// TODO(user): Modify this to be the types you create that are owned by the primary resource
 	// Watch for changes to secondary resource Pods and requeue the owner Certificate
-	err = c.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForOwner{
+	err = c.Watch(&source.Kind{Type: &certmanagerv1.Certificate{}}, &handler.EnqueueRequestForOwner{
 		IsController: true,
 		OwnerType:    &certmanagerv1alpha1.Certificate{},
 	})
@@ -126,14 +129,13 @@ func (r *ReconcileCertificate) Reconcile(request reconcile.Request) (reconcile.R
 	}
 	annotations["ibm-cert-manager-operator-generated"] = "true"
 
-	certificate := certmanagerv1.Certificate{
+	certificate := &certmanagerv1.Certificate{
 		TypeMeta: metav1.TypeMeta{Kind: "Certificate", APIVersion: "cert-manager.io/v1"},
 		ObjectMeta: metav1.ObjectMeta{
 			Name:            instance.Name,
 			Namespace:       instance.Namespace,
 			Labels:          instance.Labels,
 			Annotations:     annotations,
-			OwnerReferences: instance.OwnerReferences,
 		},
 		Spec: certmanagerv1.CertificateSpec{
 			CommonName: instance.Spec.CommonName,
@@ -148,9 +150,31 @@ func (r *ReconcileCertificate) Reconcile(request reconcile.Request) (reconcile.R
 			SecretName:  instance.Spec.SecretName,
 		},
 	}
-	if err := r.client.Create(context.TODO(), &certificate); err != nil {
+	// Set the certificate v1alpha1 as the controller of the certificate v1
+	if err := controllerutil.SetControllerReference(instance, certificate, r.scheme); err != nil {
+		reqLogger.Error(err, "### DEBUG ### failed to set Owner reference for %s", certificate)
+		return reconcile.Result{}, err
+	}
+
+	if err := r.client.Create(context.TODO(), certificate); err != nil {
 		if errors.IsAlreadyExists(err) {
-			reqLogger.Error(err, "### DEBUG ### v1 Certificate already exists")
+			existingCertificate := &certmanagerv1.Certificate{}
+			if err := r.client.Get(context.TODO(), types.NamespacedName{Namespace: certificate.Namespace, Name: certificate.Name}, existingCertificate); err != nil {
+				reqLogger.Error(err, "### DEBUG ### Failed to get v1 Certificate")
+				return reconcile.Result{}, err
+			}
+			if !equality.Semantic.DeepEqual(certificate.Labels, existingCertificate.Labels) || !equality.Semantic.DeepEqual(certificate.Spec, existingCertificate.Spec) {
+				certificate.SetResourceVersion(existingCertificate.GetResourceVersion())
+				certificate.SetAnnotations(existingCertificate.GetAnnotations())
+				if err := r.client.Update(context.TODO(), certificate); err != nil {
+					reqLogger.Error(err, "### DEBUG ### Failed to update v1 Certificate")
+					return reconcile.Result{}, err
+				}
+				reqLogger.Info("### DEBUG #### Updated v1 Certificate")
+			}
+
+			// TODO Add status update logic here
+
 			return reconcile.Result{}, nil
 		}
 		reqLogger.Error(err, "### DEBUG ### Failed to create v1 Certificate")
