db845d76293c823aa7eb3929e2ef8dd1d0f38e35
Henry Li <henry.h.li@ibm.com>
2021-08-31 14:08:40 -0400

    fixed podrefresh restarting pods too many times

--- pkg/controller/podrefresh/podrefresh_controller.go
+++ pkg/controller/podrefresh/podrefresh_controller.go
@@ -22,10 +22,8 @@ import (
 	"time"
 
 	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
@@ -40,6 +38,7 @@ import (
 var log = logf.Log.WithName("controller_podrefresh")
 var (
 	// TODO support cert-manager.io
+	expirationLabel     = "cert-manager.io/expiration"
 	restartLabel        = "certmanager.k8s.io/time-restarted"
 	noRestartAnnotation = "certmanager.k8s.io/disable-auto-restart"
 	t                   = "true"
@@ -75,41 +74,7 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 		return err
 	}
 
-	// Watch for changes to secrets in the cluster
-	err = c.Watch(&source.Kind{Type: &corev1.Secret{}}, &handler.EnqueueRequestsFromMapFunc{
-		ToRequests: &certMapper{mgr.GetClient()},
-	})
-	if err != nil {
-		return err
-	}
-
 	return nil
-}
-
-type certMapper struct {
-	client.Client
-}
-
-func (mapper *certMapper) Map(obj handler.MapObject) []reconcile.Request {
-	var requests []reconcile.Request
-
-	secrets := &corev1.SecretList{}
-
-	err := mapper.List(context.TODO(), secrets, &client.ListOptions{})
-	if err != nil {
-		return requests
-	}
-
-	for _, secret := range secrets.Items {
-		certName, ok := secret.Annotations["cert-manager.io/certificate-name"]
-		if !ok {
-			continue
-		}
-
-		requests = append(requests, reconcile.Request{NamespacedName: types.NamespacedName{Name: certName, Namespace: secret.Namespace}})
-	}
-
-	return requests
 }
 
 // blank assignment to verify that Reconcilepodrefresh implements reconcile.Reconciler
@@ -145,7 +110,7 @@ func (r *Reconcilepodrefresh) Reconcile(request reconcile.Request) (reconcile.Re
 	}
 
 	if len(cert.Status.Conditions) > 0 && cert.Status.NotAfter != nil {
-		if err := r.restart(cert.Spec.SecretName, cert.Name); err != nil {
+		if err := r.restart(cert.Spec.SecretName, cert.Name, cert.Status.NotAfter.Format("2006-1-2.1504")); err != nil {
 			reqLogger.Error(err, "Failed to fresh pod")
 			return reconcile.Result{}, err
 		}
@@ -156,41 +121,41 @@ func (r *Reconcilepodrefresh) Reconcile(request reconcile.Request) (reconcile.Re
 
 // pod refresh is enabled. It will edit the deployments, statefulsets, and daemonsets
 // that use the secret being updated, which will trigger the pod to be restarted.
-func (r *Reconcilepodrefresh) restart(secret, cert string) error {
+func (r *Reconcilepodrefresh) restart(secret, cert string, expiration string) error {
 	timeNow := time.Now().Format("2006-1-2.1504")
 	deployments := &appsv1.DeploymentList{}
 	if err := r.client.List(context.TODO(), deployments); err != nil {
 		return fmt.Errorf("error getting deployments: %v", err)
 	}
-	deploymentsToUpdate, err := r.getDeploymentsNeedUpdate(secret)
+	deploymentsToUpdate, err := r.getDeploymentsNeedUpdate(secret, expiration)
 	if err != nil {
 		return err
 	}
 
-	if err := r.updateDeploymentAnnotations(deploymentsToUpdate, cert, secret, timeNow); err != nil {
+	if err := r.updateDeploymentAnnotations(deploymentsToUpdate, cert, secret, timeNow, expiration); err != nil {
 		return err
 	}
 
-	statefulsetsToUpdate, err := r.getStsNeedUpdate(secret)
+	statefulsetsToUpdate, err := r.getStsNeedUpdate(secret, expiration)
 	if err != nil {
 		return err
 	}
-	if err := r.updateStsAnnotations(statefulsetsToUpdate, cert, secret, timeNow); err != nil {
+	if err := r.updateStsAnnotations(statefulsetsToUpdate, cert, secret, timeNow, expiration); err != nil {
 		return err
 	}
 
-	daemonsetsToUpdate, err := r.getDaemonSetNeedUpdate(secret)
+	daemonsetsToUpdate, err := r.getDaemonSetNeedUpdate(secret, expiration)
 	if err != nil {
 		return err
 	}
-	if err := r.updateDaemonSetAnnotations(daemonsetsToUpdate, cert, secret, timeNow); err != nil {
+	if err := r.updateDaemonSetAnnotations(daemonsetsToUpdate, cert, secret, timeNow, expiration); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func (r *Reconcilepodrefresh) getDeploymentsNeedUpdate(secret string) ([]appsv1.Deployment, error) {
+func (r *Reconcilepodrefresh) getDeploymentsNeedUpdate(secret, expiration string) ([]appsv1.Deployment, error) {
 	deploymentsToUpdate := make([]appsv1.Deployment, 0)
 	deployments := &appsv1.DeploymentList{}
 	if err := r.client.List(context.TODO(), deployments); err != nil {
@@ -198,6 +163,11 @@ func (r *Reconcilepodrefresh) getDeploymentsNeedUpdate(secret string) ([]appsv1.
 	}
 NEXT_DEPLOYMENT:
 	for _, deployment := range deployments.Items {
+		if deployment.ObjectMeta.Labels != nil {
+			if expiration == deployment.ObjectMeta.Labels[expirationLabel] {
+				continue
+			}
+		}
 		for _, container := range deployment.Spec.Template.Spec.Containers {
 			for _, env := range container.Env {
 				if env.ValueFrom != nil && env.ValueFrom.SecretKeyRef != nil && env.ValueFrom.SecretKeyRef.Name == secret && deployment.ObjectMeta.Annotations[noRestartAnnotation] != t {
@@ -224,7 +194,7 @@ NEXT_DEPLOYMENT:
 	return deploymentsToUpdate, nil
 }
 
-func (r *Reconcilepodrefresh) getStsNeedUpdate(secret string) ([]appsv1.StatefulSet, error) {
+func (r *Reconcilepodrefresh) getStsNeedUpdate(secret, expiration string) ([]appsv1.StatefulSet, error) {
 	statefulsetsToUpdate := make([]appsv1.StatefulSet, 0)
 	statefulsets := &appsv1.StatefulSetList{}
 	err := r.client.List(context.TODO(), statefulsets)
@@ -233,6 +203,11 @@ func (r *Reconcilepodrefresh) getStsNeedUpdate(secret string) ([]appsv1.Stateful
 	}
 NEXT_STATEFULSET:
 	for _, statefulset := range statefulsets.Items {
+		if statefulset.ObjectMeta.Labels != nil {
+			if expiration == statefulset.ObjectMeta.Labels[expirationLabel] {
+				continue
+			}
+		}
 		for _, container := range statefulset.Spec.Template.Spec.Containers {
 			for _, env := range container.Env {
 				if env.ValueFrom != nil && env.ValueFrom.SecretKeyRef != nil && env.ValueFrom.SecretKeyRef.Name == secret && statefulset.ObjectMeta.Annotations[noRestartAnnotation] != t {
@@ -259,7 +234,7 @@ NEXT_STATEFULSET:
 	return statefulsetsToUpdate, nil
 }
 
-func (r *Reconcilepodrefresh) getDaemonSetNeedUpdate(secret string) ([]appsv1.DaemonSet, error) {
+func (r *Reconcilepodrefresh) getDaemonSetNeedUpdate(secret, expiration string) ([]appsv1.DaemonSet, error) {
 	daemonsetsToUpdate := make([]appsv1.DaemonSet, 0)
 	daemonsets := &appsv1.DaemonSetList{}
 	if err := r.client.List(context.TODO(), daemonsets); err != nil {
@@ -267,6 +242,11 @@ func (r *Reconcilepodrefresh) getDaemonSetNeedUpdate(secret string) ([]appsv1.Da
 	}
 NEXT_DAEMONSET:
 	for _, daemonset := range daemonsets.Items {
+		if daemonset.ObjectMeta.Labels != nil {
+			if expiration == daemonset.ObjectMeta.Labels[expirationLabel] {
+				continue
+			}
+		}
 		for _, container := range daemonset.Spec.Template.Spec.Containers {
 			for _, env := range container.Env {
 				if env.ValueFrom != nil && env.ValueFrom.SecretKeyRef != nil && env.ValueFrom.SecretKeyRef.Name == secret && daemonset.ObjectMeta.Annotations[noRestartAnnotation] != t {
@@ -293,7 +273,7 @@ NEXT_DAEMONSET:
 	return daemonsetsToUpdate, nil
 }
 
-func (r *Reconcilepodrefresh) updateDeploymentAnnotations(deploymentsToUpdate []appsv1.Deployment, cert, secret, timeNow string) error {
+func (r *Reconcilepodrefresh) updateDeploymentAnnotations(deploymentsToUpdate []appsv1.Deployment, cert, secret, timeNow, expiration string) error {
 	for _, deployment := range deploymentsToUpdate {
 		//in case of deployments not having labels section, create the label section
 		if deployment.ObjectMeta.Labels == nil {
@@ -301,6 +281,8 @@ func (r *Reconcilepodrefresh) updateDeploymentAnnotations(deploymentsToUpdate []
 		}
 		deployment.ObjectMeta.Labels[restartLabel] = timeNow
 		deployment.Spec.Template.ObjectMeta.Labels[restartLabel] = timeNow
+		deployment.ObjectMeta.Labels[expirationLabel] = expiration
+		deployment.Spec.Template.ObjectMeta.Labels[expirationLabel] = expiration
 		err := r.client.Update(context.TODO(), &deployment)
 		if err != nil {
 			return fmt.Errorf("error updating deployment: %v", err)
@@ -310,10 +292,12 @@ func (r *Reconcilepodrefresh) updateDeploymentAnnotations(deploymentsToUpdate []
 	return nil
 }
 
-func (r *Reconcilepodrefresh) updateStsAnnotations(statefulsetsToUpdate []appsv1.StatefulSet, cert, secret, timeNow string) error {
+func (r *Reconcilepodrefresh) updateStsAnnotations(statefulsetsToUpdate []appsv1.StatefulSet, cert, secret, timeNow, expiration string) error {
 	for _, statefulset := range statefulsetsToUpdate {
 		statefulset.ObjectMeta.Labels[restartLabel] = timeNow
 		statefulset.Spec.Template.ObjectMeta.Labels[restartLabel] = timeNow
+		statefulset.ObjectMeta.Labels[expirationLabel] = expiration
+		statefulset.Spec.Template.ObjectMeta.Labels[expirationLabel] = expiration
 		if err := r.client.Update(context.TODO(), &statefulset); err != nil {
 			return fmt.Errorf("error updating statefulset: %v", err)
 		}
@@ -322,10 +306,12 @@ func (r *Reconcilepodrefresh) updateStsAnnotations(statefulsetsToUpdate []appsv1
 	return nil
 }
 
-func (r *Reconcilepodrefresh) updateDaemonSetAnnotations(daemonsetsToUpdate []appsv1.DaemonSet, cert, secret, timeNow string) error {
+func (r *Reconcilepodrefresh) updateDaemonSetAnnotations(daemonsetsToUpdate []appsv1.DaemonSet, cert, secret, timeNow, expiration string) error {
 	for _, daemonset := range daemonsetsToUpdate {
 		daemonset.ObjectMeta.Labels[restartLabel] = timeNow
 		daemonset.Spec.Template.ObjectMeta.Labels[restartLabel] = timeNow
+		daemonset.ObjectMeta.Labels[expirationLabel] = expiration
+		daemonset.Spec.Template.ObjectMeta.Labels[expirationLabel] = expiration
 		if err := r.client.Update(context.TODO(), &daemonset); err != nil {
 			return fmt.Errorf("error updating daemonset: %v", err)
 		}
