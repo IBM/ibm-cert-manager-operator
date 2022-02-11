55059a82eb507160e1ad501ed7f1c95f6818160f
Henry Li <henry.h.li@ibm.com>
2022-02-10 20:08:25 -0500

    added rbac for podrefresh controller

--- controllers/cert-manager/podrefresh_controller.go
+++ controllers/cert-manager/podrefresh_controller.go
@@ -47,9 +47,7 @@ type PodRefreshReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=cert-manager.io,resources=certificates,verbs=get;list;watch;create;update;patch;delete;deletecollection
-//+kubebuilder:rbac:groups=cert-manager.io,resources=certificates/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=cert-manager.io,resources=certificates/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=deployments;statefulsets;daemonsets,verbs=get;list;watch;create;update;patch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
