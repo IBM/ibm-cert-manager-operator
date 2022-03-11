65fbd83318ea7099352596d5c4b973056c48b8d3
Alice <zhuoxi.ibm@gmail.com>
2022-03-11 18:50:27 -0500

    create addLabel controllers

--- main.go
+++ main.go
@@ -217,6 +217,20 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "PodRefresh")
 		os.Exit(1)
 	}
+	if err = (&certmanagerv1controllers.V1AddLabelReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "V1AddLabel")
+		os.Exit(1)
+	}
+	if err = (&certmanagerv1controllers.V1Alpha1AddLabelReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "V1Alpha1AddLabel")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
