56e02350c9be7267beb2c07de7abcbb7d78b8652
Henry Li <henry.h.li@ibm.com>
2022-03-21 12:38:18 -0400

    fixed formatting in main

--- main.go
+++ main.go
@@ -99,9 +99,9 @@ func main() {
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:             scheme,
-		MetricsBindAddress: metricsAddr,
-		Port:               9443,
+		Scheme:                 scheme,
+		MetricsBindAddress:     metricsAddr,
+		Port:                   9443,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "1557e857.ibm.com",
