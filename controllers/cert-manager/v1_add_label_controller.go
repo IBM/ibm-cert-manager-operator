f00665fe96050abbc58324b76d4cf395f3ae163b
qpdpQ <liyuchen223@gmail.com>
2022-07-15 15:09:41 -0400

    add dependency
    Signed-off-by: qpdpQ <liyuchen223@gmail.com>

--- controllers/cert-manager/v1_add_label_controller.go
+++ controllers/cert-manager/v1_add_label_controller.go
@@ -18,13 +18,18 @@ package certmanager
 
 import (
 	"context"
+	"time"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	utilwait "k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/discovery"
+	"k8s.io/klog"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/log"
