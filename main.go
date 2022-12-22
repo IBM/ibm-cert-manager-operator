c208d91b80bfe22d01022af81dc67e73d6ffbad9
qpdpQ <liyuchen223@gmail.com>
2022-12-22 09:00:00 -0500

    remove mod
    Signed-off-by: qpdpQ <liyuchen223@gmail.com>

--- main.go
+++ main.go
@@ -16,14 +16,9 @@ limitations under the License.
 
 package main
 
-import (
-
-	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
-	// to ensure that exec-entrypoint and run can make use of them.
-
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
-	//+kubebuilder:scaffold:imports
-)
+// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+// to ensure that exec-entrypoint and run can make use of them.
+//+kubebuilder:scaffold:imports
 
 var ()
 
