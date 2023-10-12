fddf2d8cb0c37ea651145466bb66cbe3a4b451f7
Allen Li <liyuchen223@gmail.com>
2023-10-11 18:09:11 -0700

    change WebhookName `readOnlyRootFile`
    Signed-off-by: Allen Li <liyuchen223@gmail.com>

--- controllers/operator/deploys.go
+++ controllers/operator/deploys.go
@@ -151,7 +151,7 @@ func setupDeploy(instance *operatorv1.CertManagerConfig, deploy *appsv1.Deployme
 
 	case res.CertManagerWebhookName:
 		returningDeploy.Spec.Template.Spec.Containers[0].Image = res.GetImageID(imageRegistry, res.WebhookImageName, res.WebhookImageVersion, instance.Spec.ImagePostFix, res.WebhookImageEnvVar)
-		returningDeploy.Spec.Template.Spec.Containers[0].SecurityContext.ReadOnlyRootFilesystem = &res.FalseVar
+		returningDeploy.Spec.Template.Spec.Containers[0].SecurityContext.ReadOnlyRootFilesystem = &res.TrueVar
 		if instance.Spec.DisableHostNetwork == nil {
 			returningDeploy.Spec.Template.Spec.HostNetwork = res.FalseVar //default value
 		} else {
